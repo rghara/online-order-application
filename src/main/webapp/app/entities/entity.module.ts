fbac222e9198bd98d95ef8ebb08347c4ef0d93ab
admin <admin@RG-LPT02.futurebutler.local>
2018-10-12 23:28:56 -0400

    added online-shop entities

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,20 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { OnlinrOrderApplicationCategoryModule } from './category/category.module';
+import { OnlinrOrderApplicationProductModule } from './product/product.module';
+import { OnlinrOrderApplicationCustomerModule } from './customer/customer.module';
+import { OnlinrOrderApplicationAddressModule } from './address/address.module';
+import { OnlinrOrderApplicationWishlistModule } from './wishlist/wishlist.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        OnlinrOrderApplicationCategoryModule,
+        OnlinrOrderApplicationProductModule,
+        OnlinrOrderApplicationCustomerModule,
+        OnlinrOrderApplicationAddressModule,
+        OnlinrOrderApplicationWishlistModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
